ae4328dd40beea9cdaa70856d96e4fd9106b31a3
unknown <suren_karapetyan@edu.aua.am>
2017-09-12 22:22:48 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,12 @@
+<script>
+
+const apples=23;
+const fruit1="bananas";
+const fruit2=watermelon;
+
+console.log(apples+fruit1);
+console.log(fruit1+fruit2);
+console.log(fruit2/apples);
+
+
+</script>
